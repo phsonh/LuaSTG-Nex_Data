030c5c4cf7121c8cdca46189c068f730919d934d
Lonely Owl <benhuangxianan@gmail.com>
2026-05-07 22:55:11 +0800

    大清洗,最干净的状态

--- packages/thlib/THlib.lua
+++
@@ -1,6 +0,0 @@
--- THlib
-
-lstg.plugin.LoadPlugins()
-lstg.plugin.DispatchEvent("beforeTHlib")
-Include("THlib/THlib.lua")
-lstg.plugin.DispatchEvent("afterTHlib")
--- packages/thlib/lib/debug/AllView.lua
+++
@@ -1,4 +0,0 @@
-require("lib.debug.UpdateControllerView")
-require("lib.debug.StageDebugView")
-require("lib.debug.SelectRenderDeviceView")
-require("lib.debug.SelectAudioDeviceView")
