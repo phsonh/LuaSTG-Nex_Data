086a9061667c4a2b94b568c5a55ba7d9c7316251
Lonely Owl <benhuangxianan@gmail.com>
2026-05-08 16:42:57 +0800

    优化unit对象更新逻辑

--- mod/Test/script.lua
+++ mod/Test/script.lua
@@ -1,33 +1,32 @@
 test_unit = Class(Unit)
 
 function test_unit:init()
-    self.x = 100
-    self.y = 200
-    self.vx = 1
-    self.vy = 0.5
-    self.ax = 0
-    self.ay = 0
+    self.x = 1
+    self.y = 0
+    SetV(self, 3.1415926, 0)
     self.rot = 0
-
     Log(2, string.format(
-        "[test_unit] init id=%s x=%.2f y=%.2f",
-        tostring(self.id),
+        "[test_unit] init x=%.2f y=%.2f rot=%.2f vx=%.2f vy=%.2f",
         self.x,
-        self.y
+        self.y,
+        self.rot,
+        self.vx,
+        self.vy
     ))
 end
 
 function test_unit:frame()
     self.rot = self.rot + 1
 
-    if self.timer % 60 == 0 then
+    if self.timer % 1 == 0 then
         Log(2, string.format(
-            "[test_unit] timer=%s x=%.2f y=%.2f rot=%.2f count=%s",
+            "[test_unit] timer=%s x=%.2f y=%.2f rot=%.2f vx=%.2f vy=%.2f",
             tostring(self.timer),
             self.x,
             self.y,
             self.rot,
-            tostring(lstg.Unit.count())
+            self.vx,
+            self.vy
         ))
     end
 end
